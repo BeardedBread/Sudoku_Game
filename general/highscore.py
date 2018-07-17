d99de3b23ea1ef50d290a080529cab0cb6968b09
En Yi <randomsity1@gmail.com>
2018-07-17 15:52:28 +0800

    Add reading highscores from file

--- general/highscore.py
+++ general/highscore.py
@@ -1,1 +1,26 @@
-def ReadHighScoreFile(file):
+DIFFICULTIES = ['Very Easy', 'Easy', 'Medium', 'Hard', 'Insane']
+
+def read_highscore_file(file):
+    with open(file, 'r') as f:
+        file_data = f.read()
+
+    highscore_list = {}
+    for diff, data in zip(DIFFICULTIES, file_data.split('\n---\n')):
+        diff_list = []
+        for line in data.split('\n'):
+            info = {}
+            placing_info = line.split(',')
+            info['name'] = placing_info[0]
+            info['time'] = placing_info[1]
+            diff_list.append(info)
+        highscore_list[diff] = diff_list
+
+    return highscore_list
+
+
+def write_highscore_file(data):
+    pass
+
+
+if __name__ == "__main__":
+    score = read_highscore_file("./highscore.txt")
